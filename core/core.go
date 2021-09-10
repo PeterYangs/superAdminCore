83391953bf26bb7f72481087b1ec03cf37bf98d6
PeterYangs <904801074@qq.com>
2021-09-10 16:31:49 +0800

    安全退出支持

--- core/core.go
+++ core/core.go
@@ -1,6 +1,9 @@
 package core
 
 import (
+	"context"
+	"fmt"
+	"github.com/PeterYangs/superAdminCore/component/logs"
 	conf2 "github.com/PeterYangs/superAdminCore/conf"
 	"github.com/PeterYangs/superAdminCore/route"
 	"github.com/gin-gonic/gin"
@@ -8,10 +11,21 @@ import (
 	"log"
 	http_ "net/http"
 	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
 )
 
 type Core struct {
-	Engine *gin.Engine
+	Engine   *gin.Engine
+	Cxt      context.Context
+	Cancel   context.CancelFunc
+	Wait     *sync.WaitGroup
+	HttpOk   chan bool
+	httpFail chan bool
+	Sigs     chan os.Signal
+	Srv      *http_.Server
 }
 
 func NewCore() *Core {
@@ -22,8 +36,29 @@ func NewCore() *Core {
 		panic("配置文件加载失败")
 	}
 
+	//检测退出信号
+	sigs := make(chan os.Signal, 1)
+
+	//退出信号
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	//服务退出上下文，主要作用是让其他子组件协程安全退出
+	cxt, cancel := context.WithCancel(context.Background())
+
+	wait := sync.WaitGroup{}
+
+	httpOk := make(chan bool)
+
+	httpFail := make(chan bool)
+
 	return &Core{
-		Engine: gin.Default(),
+		Engine:   gin.Default(),
+		Cxt:      cxt,
+		Cancel:   cancel,
+		Wait:     &wait,
+		HttpOk:   httpOk,
+		httpFail: httpFail,
+		Sigs:     sigs,
 	}
 }
 
@@ -43,7 +78,40 @@ func (core *Core) LoadConf(conf map[string]interface{}) {
 
 func (core *Core) Start() {
 
+	go core.quitCheck()
+
+	core.logInit()
+
+	go core.http()
+
+	//等待其他子组件服务退出
+	core.Wait.Wait()
+
+	fmt.Println("finish")
+
+}
+
+//--------------------------------------------------------
+
+//启动日志服务
+func (core *Core) logInit() {
+
+	//日志退出标记
+	//wait.Add(1)
+	core.Wait.Add(1)
+
+	l := logs.CreateLogs()
+
+	//日志写入任务
+	go l.Task(core.Cxt, core.Wait)
+
+}
+
+func (core *Core) http() {
+
 	srv := &http_.Server{}
+
+	core.Srv = srv
 
 	//设置端口
 	port := os.Getenv("PORT")
@@ -63,7 +131,40 @@ func (core *Core) Start() {
 
 		//http服务启动失败
 		//httpFail <- true
+		core.httpFail <- true
 
 	}
+
+}
+
+func (core *Core) quitCheck() {
+
+	sig := <-core.Sigs
+
+	fmt.Println()
+	fmt.Println(sig)
+
+	//if isRun {
+
+	//删除pid文件
+	_ = os.Remove("logs/run.pid")
+
+	//}
+
+	c, e := context.WithTimeout(context.Background(), 3*time.Second)
+
+	defer e()
+
+	//关闭http服务
+	err := core.Srv.Shutdown(c)
+
+	if err != nil {
+
+		log.Println(err)
+	}
+
+	//通知子组件协程退出
+	//cancel()
+	core.Cancel()
 
 }
